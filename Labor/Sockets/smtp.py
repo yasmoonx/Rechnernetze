65e8daaa8176d8d9e3f19c700985953105b8c4db
Yasmin <yasminhoffmann99@web.de>
2022-12-13 23:39:52 +0100

    code cleanup, avoid redundancy by adding code check method

--- Labor/Sockets/smtp.py
+++ Labor/Sockets/smtp.py
@@ -1,100 +1,87 @@
 import base64
 import socket
 
+username = base64.b64encode('rnetin'.encode('utf-8')).decode('utf-8')
+password = base64.b64encode(
+    'Ueben8fuer8RN'.encode('utf-8')).decode('utf-8')
+mailserver = 'asmtp.htwg-konstanz.de'
+port = 587
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((mailserver, port))
+
+
 def main():
     nl = '\r\n'
     print('Enter data manually or press enter for default values')
-    sender= input('Sender mail address:')
+    sender = input('Sender mail address:')
     if(sender == ''):
-        sender= "scammer@scam.de"
+        sender = "scammer@scam.de"
         print('Default:'+sender)
-    rcpt= input('Recipient mail address:')
+    rcpt = input('Recipient mail address:')
     if(rcpt == ''):
-        rcpt= 'yasmin.hoffmann@htwg-konstanz.de' 
+        rcpt = 'yasmin.hoffmann@htwg-konstanz.de'
         print('Default:'+rcpt)
-    subject= input('Email subject:')
+    subject = input('Email subject:')
     if(subject == ''):
-        subject= 'irrelevant'
+        subject = 'irrelevant'
         print('Default:'+subject)
-    content= input('Email content:')
-    if(content==''):
-        content= 'This is a scam email from me muahaha' 
+    content = input('Email content:')
+    if(content == ''):
+        content = 'This is a scam email from me muahaha'
         print('Default:'+content)
 
-
-    username = base64.b64encode('rnetin'.encode('utf-8')).decode('utf-8') 
-    password = base64.b64encode('Ueben8fuer8RN'.encode('utf-8')).decode('utf-8') 
-    mailserver = 'asmtp.htwg-konstanz.de'
-    port= 587
-
-
-    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect((mailserver, port))
-    ans = sock.recv(1024).decode()
-    if(ans[:3]!= '220' ):
-        print('Server not ready! (no 220 answer)')
-
+    checkCode('220')  # initial server request
 
     sock.send(('EHLO example.net'+nl).encode())
-    ans = sock.recv(1024).decode()
-    if(ans[:3]!= '250'):
-        print('no 250 answer')
-
+    checkCode('250')
 
     sock.send(('AUTH LOGIN'+nl).encode())
-    ans= sock.recv(1024).decode()
-    if(ans[:3]!= '334'):
-        print('no 334 answer')
+    checkCode('334')
 
     sock.send((username+nl).encode())
-    ans= sock.recv(1024).decode()
-    if(ans[:3]!= '334'):
-        print('no 334 answer')
+    checkCode('334')
 
     sock.send((password+nl).encode())
-    ans= sock.recv(1024).decode()
-    if(ans[:3]!= '235'):
-        print('no 235 answer')
+    checkCode('235')
 
     sock.send(('MAIL FROM: <'+sender+'>'+nl).encode())
-    ans= sock.recv(1024).decode()
-    if(ans[:3]!= '250'):
-        print('no 250 answer')
+    checkCode('250')
 
     sock.send(('RCPT TO: <'+rcpt+'>'+nl).encode())
-    ans= sock.recv(1024).decode()
-    if(ans[:3]!= '250'):
-        print('no 250 answer')
+    checkCode('250')
 
     sock.send(('DATA'+nl).encode())
-    ans= sock.recv(1024).decode()
-    if(ans[:3]!= '354'):
-        print('no 354 answer')
+    checkCode('354')
 
-    msg= 'FROM:<' + sender +'>'+ nl
-    msg += 'TO:<' + rcpt +'>' + nl
+    msg = 'FROM:<' + sender + '>' + nl
+    msg += 'TO:<' + rcpt + '>' + nl
     msg += 'Subject:' + subject + nl
     msg += nl
     msg += content + nl
     msg += '.' + nl
 
     sock.send(msg.encode())
-    ans= sock.recv(1024).decode()
-    if(ans[:3] != '250'):
-        print('no 250 answer')
-    else: 
+
+    if checkCode('250'):
         print('Email was sent successfully')
+    else: 
+        print('Sorry, email was not sent.')
 
     sock.send(('QUIT'+nl).encode())
-    ans= sock.recv(1024).decode()
-    if(ans[:3] != '221'):
-        print('no 221 answer')
-    else:
+    checkCode('221')
+
+
+def checkCode(code):
+    ans = sock.recv(1024).decode()
+    if(ans[:3] != code):
+        print('Error! (no ' + code + ' answer)')
         print(ans)
+        return 0
+    else: 
+        print(ans)
+        return 1
+
 
 if __name__ == "__main__":
     main()
-
-
-       
-
